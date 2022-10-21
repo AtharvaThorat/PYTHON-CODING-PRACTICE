7d044a0494a64363fc8c463b35e311c1e9e297e1
AtharvaThorat <101087445+AtharvaThorat@users.noreply.github.com>
2022-10-21 12:15:33 +0530

    Add files via upload

---
+++ set_operations.py
@@ -0,0 +1,16 @@
+#Python program to illustrate different set operations
+#two sets A and B
+A = {0, 2, 4, 6, 8};
+B = {1, 2, 3, 4, 5};
+  
+# union of E and N
+print("Union :", A | B)
+  
+# intersection of E and N
+print("Intersection :", A & B)
+  
+# difference of E and N
+print("Difference :", A - B)
+  
+# symmetric difference of E and N
+print("Symmetric difference :", A ^ B)
